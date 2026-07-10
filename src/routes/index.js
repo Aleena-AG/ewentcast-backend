9a28440c08f04d60d819ed975e63bf208cc14094
Muaz Munir <muazmuneer151m@gmail.com>
2026-07-10 16:08:52 +0500

    reset to default api structure

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const healthRoutes = require("./health.routes");
 const userRoutes = require("./user.routes");
 
 const router = express.Router();
 
-router.get("/health", (req, res) => res.json({ success: true, message: "API is running" }));
+router.use("/health", healthRoutes);
 router.use("/users", userRoutes);
 
 module.exports = router;
