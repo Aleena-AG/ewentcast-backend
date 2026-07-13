184739a9508c16aebcf864218b0d006de205450c
Aleena <aleenaghafoor239@gmail.com>
2026-07-13 11:57:28 +0400

    build error

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = require("./src/app");
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// Local / non-Vercel: start HTTP server. On Vercel, export the app as a function.
+if (process.env.VERCEL !== "1") {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
